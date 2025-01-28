88dfaea706a8196fffcc799d93a989e2b0693cf9
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2025-01-27 21:49:59 -0300

    Documentação no Readme

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1>Enviando para o Github</h1>
-      <p>Atividade finalizada!</p>
+      <p>Atividade em andamento...</p>
+      <p>Testando uma nova versão</p>
     </div>
   );
 }
